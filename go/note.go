979bece3990a289fb76f827836f0e4d8d68330be
mahaoyuan <1440970573@qq.com>
2022-09-08 16:25:37 +0800

    add: heap

--- go/note.go
+++ go/note.go
@@ -2,11 +2,37 @@ package note
 
 import (
 	"fmt"
+	"mhy/utils"
 	"unsafe"
 )
 
 type s1 struct{ Val int }
 type s2 struct{ Val int }
+
+type ListNode = utils.SListNode
+
+type ListNodeHeap []*ListNode
+
+func (h ListNodeHeap) Len() int {
+	return len(h)
+}
+func (h ListNodeHeap) Less(i, j int) bool {
+	return h[i].Val < h[j].Val
+}
+func (h ListNodeHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *ListNodeHeap) Push(x interface{}) {
+	*h = append(*h, x.(*ListNode))
+}
+func (h *ListNodeHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
 
 func note() {
 	var a interface{}
