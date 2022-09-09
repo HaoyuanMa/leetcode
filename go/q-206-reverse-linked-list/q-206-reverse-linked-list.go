c584392789fdd355fe14c557db9c3f9e358106e5
mahaoyuan <1440970573@qq.com>
2022-09-09 15:39:39 +0800

    add: q206

---
+++ go/q-206-reverse-linked-list/q-206-reverse-linked-list.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"mhy/utils"
+)
+
+//206.反转链表.reverse-linked-list
+
+type ListNode = utils.SListNode
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	// without t
+	// head = head.Next.Next
+
+	t := head.Next
+	res := reverseList(head.Next)
+	head.Next = nil
+	t.Next = head
+	return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func main() {
+	a := utils.CreatSingleList([]int{1, 2, 3, 4, 5})
+	result := reverseList(a)
+	utils.PrintList(result)
+}
