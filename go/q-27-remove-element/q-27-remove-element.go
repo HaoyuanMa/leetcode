2f9f50d58cec5da8e2af99ace781bedd14934dcc
mahaoyuan <1440970573@qq.com>
2022-09-14 10:05:21 +0800

    update: q27

--- go/q-27-remove-element/q-27-remove-element.go
+++ go/q-27-remove-element/q-27-remove-element.go
@@ -8,17 +8,14 @@ import "fmt"
 func removeElement(nums []int, val int) int {
 	l := len(nums)
 	ans := l
-	p, q := 0, 1
+	p, q := 0, 0
 	for q < l {
-		if nums[p] != val {
+		if nums[q] != val {
+			nums[p] = nums[q]
 			p++
 			q++
 		} else if nums[q] == val {
 			ans--
-			q++
-		} else {
-			nums[p] = nums[q]
-			p++
 			q++
 		}
 	}
@@ -28,7 +25,7 @@ func removeElement(nums []int, val int) int {
 //leetcode submit region end(Prohibit modification and deletion)
 
 func main() {
-	a := []int{1, 1, 2, 3, 3, 4, 4, 5, 6, 7, 7}
+	a := []int{3, 1, 1, 3, 2, 3, 3, 4, 4, 5, 6, 7, 7}
 	result := removeElement(a, 3)
 	fmt.Println(result)
 	fmt.Println(a)
