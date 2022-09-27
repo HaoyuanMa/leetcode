29239cef4ebc322ce4c4a1e7d9274795d18c2bcd
mahaoyuan <1440970573@qq.com>
2022-09-27 12:50:26 +0800

    add:q17.19

---
+++ go/q-17.19-missing-two-lcci/q-17.19-missing-two-lcci.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//面试题 17.19.消失的两个数字.missing-two-lcci
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func missingTwo(nums []int) []int {
+	n := len(nums)
+	a, b := 0, 0
+	for i := 0; i < n; i++ {
+		a += i - nums[i]
+		b += i*i - nums[i]*nums[i]
+	}
+	a += n + n + 1 + n + 2
+	b += 3*n*n + 6*n + 5
+	sum := a
+	product := (a*a - b) / 2
+	dt := sum*sum - 4*product
+	dt_sqrt := int(math.Sqrt(float64(dt)))
+	x := (sum + dt_sqrt) / 2
+	y := sum - x
+	return []int{x, y}
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func main() {
+	a := []int{1}
+	result := missingTwo(a)
+	fmt.Println(result)
+}
